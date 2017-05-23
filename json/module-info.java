ecbda1d739ecaa41a9d474336c8185e3a706306c
Peter Steiner <unistein+reinette@gmail.com>
2017-05-23 10:22:43 +0200

    first working "mvn clean install" with jdk-9-ea+168

---
+++ json/module-info.java
@@ -0,0 +1,7 @@
+module ch.schlau.pesche.snppts.json {
+    requires java.base;
+    requires java.xml.bind;
+    requires commons.lang3;
+    requires jackson.core;
+    requires jackson.databind;
+}
