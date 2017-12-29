57bdf224b5bd1f402e1dfe3ff79ca4fbb62a6b7f
Peter Steiner <unistein+n38151@gmail.com>
2017-12-29 16:07:01 +0100

    Add a test for stringFromResourceFile

---
+++ testing/src/test/java/ch/schlau/pesche/snppts/testing/ResourceUtilsTest.java
@@ -0,0 +1,16 @@
+package ch.schlau.pesche.snppts.testing;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ResourceUtilsTest {
+
+    @Test
+    public void stringFromResourceFile_42() {
+
+        ResourceUtils resourceUtils = new ResourceUtils();
+        assertThat(resourceUtils.stringFromResourceFile("42.txt"), is("42"));
+    }
+}
